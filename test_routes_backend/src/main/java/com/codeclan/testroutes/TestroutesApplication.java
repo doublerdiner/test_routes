f0ded22166b10fb745311c0e59e8b5595d843768
Chris Barclay <chris.barclay1@gmail.com>
2023-05-05 09:53:49 +0100

    backend added

---
+++ test_routes_backend/src/main/java/com/codeclan/testroutes/TestroutesApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.testroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestroutesApplication.class, args);
+	}
+
+}
---
+++ test_routes_backend/src/test/java/com/codeclan/testroutes/TestroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.testroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
